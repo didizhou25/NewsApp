033f215406fc6192725340a5bd39f16096fe2f92
0925177 <didizhou25@hotmail.com>
2019-01-28 09:40:23 +0100

    Laravel finish

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,5 +2,7 @@
 
 @section('content')
     <h1>{{$title}}</h1>
-    <p>Welcome to the NewsApp, you can start by making your own account, to add new posts aswell!</p>
+    <p>Here you can start by making your own account, to add new posts aswell!<br>
+        You can also view posts made by other users.
+    </p>
 @endsection
